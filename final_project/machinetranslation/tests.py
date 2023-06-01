2c8fe0604a7ee9b3a0017acd4c79e60895a8bfd2
monoplop <andrews.stephen.7734+monoplop@gmail.com>
2023-06-01 11:28:40 -0400

    added tests, passed

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,4 +1,12 @@
 import unittest
-from translator import englishToFrench
-from translator import frenchToEnglish
+from translator import englishToFrench, frenchToEnglish
 
+class TestEnglishToFrench(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(englishToFrench('hello'), 'bonjour') # test that hello translates to bonjour.
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(frenchToEnglish('bonjour'),'hello') # test that bonjour translates to hello.
+
+unittest.main()
